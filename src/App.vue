d18d50fadf53abe5cec29ca59f96b17d70a7cce4
voonyc <voonyc@sains.com.my>
2023-07-21 08:44:31 +0800

    add image

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <org-chart :datasource="ds" @node-click="selectNode">
-<!--       <template slot-scope="{ nodeData }">
+      <!--       <template slot-scope="{ nodeData }">
         <b @click="selectNode(nodeData)">{{ nodeData.name }}</b>
       </template> -->
     </org-chart>
@@ -9,49 +9,97 @@
 </template>
 
 <script>
-import OrgChart from './components/OrganizationChartContainer.vue'
+import OrgChart from "./components/OrganizationChartContainer.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    OrgChart
+    OrgChart,
   },
-  data () {
+  data() {
     return {
       ds: {
-        'id': '1',
-        'name': 'Lao Lao',
-        'title': 'general manager',
-        'children': [
-          { 'id': '2', 'name': 'Bo Miao', 'title': 'department manager' },
-          { 'id': '3', 'name': 'Su Miao', 'title': 'department manager',
-            'children': [
-              { 'id': '4', 'name': 'Tie Hua', 'title': 'senior engineer' },
-              { 'id': '5', 'name': 'Hei Hei', 'title': 'senior engineer',
-                'children': [
-                  { 'id': '6', 'name': 'Pang Pang', 'title': 'engineer' },
-                  { 'id': '7', 'name': 'Xiang Xiang', 'title': 'UE engineer' }
-                ]
-               }
-             ]
-           },
-          { 'id': '8', 'name': 'Hong Miao', 'title': 'department manager' },
-          { 'id': '9', 'name': 'Chun Miao', 'title': 'department manager' }
-        ]
-      }
-    }
+        id: "1",
+        name: "Lao Lao",
+        title: "general manager",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+        children: [
+          {
+            id: "2",
+            name: "Bo Miao",
+            title: "department manager",
+            image:
+              "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+          },
+          {
+            id: "3",
+            name: "Su Miao",
+            title: "department manager",
+            image:
+              "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+            children: [
+              {
+                id: "4",
+                name: "Tie Hua",
+                title: "senior engineer",
+                image:
+                  "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+              },
+              {
+                id: "5",
+                name: "Hei Hei",
+                title: "senior engineer",
+                image:
+                  "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+                children: [
+                  {
+                    id: "6",
+                    name: "Pang Pang",
+                    title: "engineer",
+                    image:
+                      "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+                  },
+                  {
+                    id: "7",
+                    name: "Xiang Xiang",
+                    title: "UE engineer",
+                    image:
+                      "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+                  },
+                ],
+              },
+            ],
+          },
+          {
+            id: "8",
+            name: "Hong Miao",
+            title: "department manager",
+            image:
+              "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+          },
+          {
+            id: "9",
+            name: "Chun Miao",
+            title: "department manager",
+            image:
+              "https://upload.wikimedia.org/wikipedia/commons/3/34/Elon_Musk_Royal_Society_%28crop2%29.jpg",
+          },
+        ],
+      },
+    };
   },
   methods: {
-    selectNode (nodeData) {
-      alert('node ' + nodeData.name + ' is selected')
-    }
-  }
-}
+    selectNode(nodeData) {
+      alert("node " + nodeData.name + " is selected");
+    },
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/OrganizationChartNode.vue
+++ src/components/OrganizationChartNode.vue
@@ -2,37 +2,52 @@
   <table>
     <tbody>
       <tr>
-       <td :colspan="datasource.children && datasource.children.length ? datasource.children.length*2 : null">
-        <div class="node" :id="datasource.id" @click.stop="handleClick(datasource)">
-          <slot :node-data="datasource">
-            <div class="title">
-              <i class="fa fa-users symbol"></i>
-              {{ datasource.name }}
-            </div>
-            <div class="content">{{ datasource.title }}</div>
-          </slot>
-        </div>
-       </td>
+        <td
+          :colspan="
+            datasource.children && datasource.children.length
+              ? datasource.children.length * 2
+              : null
+          "
+        >
+          <div
+            class="node"
+            :id="datasource.id"
+            @click.stop="handleClick(datasource)"
+          >
+            <slot :node-data="datasource">
+             <img :src="datasource.image" alt="Avatar" class="avatar">
+              <div class="title">
+                <i class="fa fa-users symbol"></i>
+                {{ datasource.name }}
+              </div>
+              <div class="content">{{ datasource.title }}</div>
+            </slot>
+          </div>
+        </td>
       </tr>
       <template v-if="datasource.children && datasource.children.length">
         <tr class="lines">
-          <td :colspan="datasource.children.length*2">
+          <td :colspan="datasource.children.length * 2">
             <div class="downLine"></div>
           </td>
         </tr>
         <tr class="lines">
           <td class="rightLine"></td>
-         <template v-for="n in (datasource.children.length-1)">
+          <template v-for="n in datasource.children.length - 1">
             <td class="leftLine topLine"></td>
             <td class="rightLine topLine"></td>
-         </template>
+          </template>
           <td class="leftLine"></td>
         </tr>
         <tr class="nodes">
           <td colspan="2" v-for="child in datasource.children" :key="child.id">
             <node :datasource="child" :handle-click="handleClick">
-              <template v-for="slot in Object.keys($scopedSlots)" :slot="slot" slot-scope="scope">
-                <slot :name="slot" v-bind="scope"/>
+              <template
+                v-for="slot in Object.keys($scopedSlots)"
+                :slot="slot"
+                slot-scope="scope"
+              >
+                <slot :name="slot" v-bind="scope" />
               </template>
             </node>
           </td>
@@ -44,15 +59,20 @@
 
 <script>
 export default {
-  name: 'node',
+  name: "node",
   props: {
     datasource: Object,
-    handleClick: Function
+    handleClick: Function,
   },
-  methods: {
-  }
+  methods: {},
 };
 </script>
 
 <style>
+.avatar {
+  vertical-align: middle;
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+}
 </style>
